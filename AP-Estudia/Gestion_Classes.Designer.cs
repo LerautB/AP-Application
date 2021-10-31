f7ea07665a3b7b1c62efbda38c12da8bec5c02d2
Bastien Leraut <bastien.leraut@free.fr>
2021-10-31 18:31:06 +0100

    first commit

---
+++ AP-Estudia/Gestion_Classes.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace AP_Estudia
+{
+    partial class Gestion_Classes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Gestion_Classes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(1276, 667);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Gestion_Classes";
+            this.Text = "Gestion_Classes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ AP-Estudia/login.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BCrypt.Net;
+using MySql.Data.MySqlClient;
+
+namespace AP_Estudia
+{
+    using BCrypt.Net;
+    
+    
+    public partial class login : Form
+    {
+        MySqlConnection conn = new MySqlConnection("database=estudia; server=localhost; user id = root; pwd=");
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mot_de_passe = mdp_input.Text;
+            string identifiant = id_input.Text;
+            string User_mdp = "";
+            string User_nom;
+            string User_statut;
+            conn.Open();
+            MySqlCommand command = conn.CreateCommand();
+            
+            command.Parameters.AddWithValue("@id", identifiant);
+            
+
+            command.CommandText = "SELECT nom,mdp,statut FROM utilisateur WHERE identifiant = @id";
+            
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                User_nom = reader.GetString(0);
+                User_mdp = reader.GetString(1);
+                User_statut = reader.GetString(2);
+                
+            }
+            // Fermeture de la connexion
+            conn.Close();
+
+            if(BCrypt.Verify(mot_de_passe, User_mdp))
+            {
+                MessageBox.Show("Connexion réussi...");
+            }
+            else
+            {
+                MessageBox.Show("Connexion échouée...");
+            }
+        }
+    }
+}
